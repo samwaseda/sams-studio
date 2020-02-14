0ec1dbac26d54549224de62cca65ab9dfd789e9f
Osamu Waseda <o.waseda@mpie.de>
2020-02-14 09:15:02 +0100

    formatting

--- AKMC/main.cpp
+++ AKMC/main.cpp
@@ -26,8 +26,8 @@ void error_exit(string str)
 
 void warning(bool condition, string message)
 {
-    if(condition)
-        cout<<"WARNING: "<<message<<endl;
+	if(condition)
+		cout<<"WARNING: "<<message<<endl;
 }
 
 class Energy{
@@ -62,19 +62,19 @@ class Energy{
 			return deltaE;
 		}
 
-        void set_temperature(double temperature, double tau=1.0e-13)
-        {
-            assert(("temperature must be a positive float", temperature>0));
-            assert(("tau must be a positive float", tau>0));
-            kBT = 8.617e-5*temperature;
-            logtau = log(tau);
-        }
+		void set_temperature(double temperature, double tau=1.0e-13)
+		{
+			assert(("temperature must be a positive float", temperature>0));
+			assert(("tau must be a positive float", tau>0));
+			kBT = 8.617e-5*temperature;
+			logtau = log(tau);
+		}
 
-        double return_kappa(double *sss)
-        {
-            assert(("Temperature and tau have not been set", kBT>0 && logtau>0));
-            return exp(-calc_energy(sss)/kBT-logtau);
-        }
+		double return_kappa(double *sss)
+		{
+			assert(("Temperature and tau have not been set", kBT>0 && logtau>0));
+			return exp(-calc_energy(sss)/kBT-logtau);
+		}
 }energy;
 
 class Atom{
@@ -172,17 +172,17 @@ class AKMC{
 		double a_0;
 		int N_octa, N_C;
 		Eigen::Matrix3d tmat;
-        Node *head = new Node;
+		Node *head = new Node;
 	public:
 		AKMC(double dislocation_density, string input_file, double temperature_in,
-             double box_height, double number_of_C_atoms, vector<double>core_position,
-             double r_core, double r_kmc) : a_0(2.855312531), N_octa(0)
+			 double box_height, double number_of_C_atoms, vector<double>core_position,
+			 double r_core, double r_kmc) : a_0(2.855312531), N_octa(0)
 		{
 			cout<<"AKMC initialization"<<endl;
 			tmat << 1.0/sqrt(3), -1.0/sqrt(2), 1.0/sqrt(6),
 					1.0/sqrt(3), 0, -2.0/sqrt(6),
 					1.0/sqrt(3), 1.0/sqrt(2),  1.0/sqrt(6);
-            energy.set_temperature(temperature_in);
+			energy.set_temperature(temperature_in);
 			set_box(box_height, dislocation_density);
 			set_number_of_C_atoms(number_of_C_atoms);
 			read_octa(input_file, core_position, r_core, r_kmc);
@@ -201,9 +201,9 @@ class AKMC{
 
 		double read_octa(string input_file, vector<double> core_position, double r_core, double r_kmc)
 		{
-            assert(("core_position has to be x,y-coordinates", core_position.size()!=2));
-            warning(abs(core_position.at(0))+abs(core_position.at(1))!=0, "core_position shift not implemented");
-            assert(("r_core must be smaller than or equal to r_kmc", r_core<=r_kmc));
+			assert(("core_position has to be x,y-coordinates", core_position.size()!=2));
+			warning(abs(core_position.at(0))+abs(core_position.at(1))!=0, "core_position shift not implemented");
+			assert(("r_core must be smaller than or equal to r_kmc", r_core<=r_kmc));
 			fstream eingabe;
 			eingabe.open(input_file, ios::in);
 			if(!eingabe)
@@ -229,9 +229,9 @@ class AKMC{
 				for(int j=0; j<3 && ss>>x[j]; j++);
 				ss>>id_NN;
 				for(int j=0; j<6 && ss>>S[j]; j++);
-                assert(("ID of neighbor is the ID of itself", id_self!=id_NN));
-                assert(("neighbor ID larger than number of sites", max(id_self, id_NN)<N_octa));
-                assert(("negative ID makes no sense", min(id_NN, id_self)>=0));
+				assert(("ID of neighbor is the ID of itself", id_self!=id_NN));
+				assert(("neighbor ID larger than number of sites", max(id_self, id_NN)<N_octa));
+				assert(("negative ID makes no sense", min(id_NN, id_self)>=0));
 				octa[id_self].set_position(x);
 				octa[id_self].set_neighbor(id_NN, S);
 				octa[id_self].set_cell(&cell);
@@ -242,8 +242,8 @@ class AKMC{
 
 		void set_box(double box_height, double dislocation_density)
 		{
-            assert(("Problem with the box height", box_height>0));
-            assert(("Dislocation density has to be a positive float", dislocation_density>0));
+			assert(("Problem with the box height", box_height>0));
+			assert(("Dislocation density has to be a positive float", dislocation_density>0));
 			cell<<1.0/sqrt(dislocation_density), 1.0/sqrt(dislocation_density), box_height;
 			cout<<"Box size: "<<cell.prod()<<endl;
 		}
@@ -267,7 +267,7 @@ int main(int arg, char **name){
 	cxxopts::Options options(name[0], "AKMC code");
 	int rand_seed=0, nsteps=10000000;
 	double temperature=300, dislocation_density=1.0e-6, box_height=27.9762, number_of_C_atoms=1, r_core=10, r_kmc=120;
-    vector<double> core_position (2, 0);
+	vector<double> core_position (2, 0);
 	string octa_config="octa.dat", output_file="akmc.log";
 	options.add_options()
 		("T, temperature", "Temperature K (default: 300 K)", cxxopts::value<double>(temperature))
@@ -277,7 +277,7 @@ int main(int arg, char **name){
 		("o, output", "Output file name (default: akmc.log)", cxxopts::value<string>(output_file))
 		("i, input", "Input file name (default: octa.dat)", cxxopts::value<string>(octa_config))
 		("z, height", "Box height (default: 27.9762)", cxxopts::value<double>(box_height))
-        ("c, core", "Core position in x,y (default: 0, 0)", cxxopts::value<vector<double> >(core_position))
+		("c, core", "Core position in x,y (default: 0, 0)", cxxopts::value<vector<double> >(core_position))
 		("r, radius_core", "Radius of the core region in A (default: 10)", cxxopts::value<double>(r_core))
 		("R, radius_kmc", "Radius of the KMC region in A (default: 120)", cxxopts::value<double>(r_kmc))
 		("h, help", "Print help")
@@ -287,7 +287,7 @@ int main(int arg, char **name){
 	if (results.count("help"))
 	{
 		cout<<options.help({"", "Group"})<<endl;
-        return 0;
+		return 0;
 	}
 	AKMC akmc = AKMC(dislocation_density, octa_config, temperature, box_height, number_of_C_atoms, core_position, r_core, r_kmc);
 }
